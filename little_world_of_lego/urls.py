d66efc8f8c2f6a0a9b8fa34c052c3cf0a1f618bf
AndrewH1188 <andrewh1188@gmail.com>
2022-03-21 22:48:48 +0000

    Home page layout and styling with icons now showing

--- little_world_of_lego/urls.py
+++ little_world_of_lego/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Code has been used from the Code Institute Boutique Ado
+# Walkthrough project
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
