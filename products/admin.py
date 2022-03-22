df36e44cbf88336a00727c54f782cd78b6c1cc21
AndrewH1188 <andrewh1188@gmail.com>
2022-03-22 14:40:02 +0000

    Adding products and apps for these

---
+++ products/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+
+# Code has been used from the Code Institute Boutique Ado
+# Walkthrough project
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
