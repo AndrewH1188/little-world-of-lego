70e9150167a98cd2bf9c1cb5dc1000e2aae1ed0e
AndrewH1188 <andrewh1188@gmail.com>
2022-03-24 17:51:55 +0000

    Shop now links added, basket done but error somewhere to be fixed

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 # Code has been used from the Code Institute Boutique Ado
 # Walkthrough project
@@ -9,6 +11,17 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -17,7 +30,7 @@ def basket_contents(request):
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery = delivery + total
+    grand_total = delivery + total
 
     context = {
         """ Displays information to a user with products in their basket """
@@ -27,7 +40,7 @@ def basket_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total
+        'grand_total': grand_total,
     }
 
     return context
