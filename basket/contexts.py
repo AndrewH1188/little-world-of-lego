d15a15ccdefe269b53244b290e411cd4a2f82f26
AndrewH1188 <andrewh1188@gmail.com>
2022-03-25 11:36:13 +0000

    Basket error fixed so items in basket now display with product text, price etc

--- basket/contexts.py
+++ basket/contexts.py
@@ -13,15 +13,17 @@ def basket_contents(request):
     product_count = 0
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+
+    for item_id, item_data in basket.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -33,7 +35,6 @@ def basket_contents(request):
     grand_total = delivery + total
 
     context = {
-        """ Displays information to a user with products in their basket """
         'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
