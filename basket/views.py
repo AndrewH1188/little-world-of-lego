9d81ef396e7e0fa92459cc673ae8032ce75ae52e
AndrewH1188 <andrewh1188@gmail.com>
2022-03-26 14:00:27 +0000

    Update quantity in bag functionality added, remove product to work on

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Code has been used from the Code Institute Boutique Ado
 # Walkthrough project
@@ -24,3 +24,37 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjust a quantity of the product selected in our basket """
+
+    quantity = int(request.POST.get('quantity'))
+    basket=request.session.get('basket',{})
+    
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+
+# Not working just yet - look into this
+def remove_from_basket(request, item_id):
+    """ Remove a product from our basket """
+    try:
+        if 'product' in request.POST:
+            basket=request.session.get('basket',{})
+
+        del basket[item_id]
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
