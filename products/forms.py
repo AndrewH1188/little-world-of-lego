18f4f21b37c25d3416a3ca18b1983d36eb472ea4
AndrewH1188 <andrewh1188@gmail.com>
2022-04-03 21:29:20 +0000

    Setting up widgets for the Product Management page

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 # Code has been used from the Code Institute Boutique Ado
@@ -9,6 +10,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
